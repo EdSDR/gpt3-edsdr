db6ef56cad110ddde1aa0e7153e853f7a169828c
Ed Castro <ed.sdr@outlook.com>
2021-11-25 13:57:49 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="gradient_bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
